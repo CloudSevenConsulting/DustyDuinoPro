b4862b3763ccfb37138b98daa8efc09c320dc9fb
Jamie Phan <jamie.kt.phan@gmail.com>
2017-09-05 17:21:57 +0800

    Add skeleton for Test conditional build
    The main routine can be built in 'test' mode with using the pre-
    -processor. This is to ensure that production and testing
    environments are independent of one another; saving memory on chip,
    ensuring independence in operation, and maintaining the code base.

---
+++ src/C7C.Atamo.Dusty.Mote/Tester/tester.c
@@ -0,0 +1,76 @@
+/* ============================================================================
+ * C7C.Atamo.Dusty.Mote.Tester.tester.c
+ * ------------------------------------
+ *
+ * -------
+ * PURPOSE
+ * -------
+ *      Main routine for running all tests
+ *
+ * ------------------
+ * DEVELOPMENT STATUS
+ * ------------------
+ *      Last Updated: 2017-09-04
+ *      Last Editor: Phan, J.
+ *
+ *      Created
+ *
+ * ----------------
+ * ADDITIONAL NOTES
+ * ----------------
+ *      Setting the #defines to 1 in the 'Test Selection' section will enable
+ *      the test routines in build. Setting them to 0 will result in the pre-
+ *      -processor ignoring the test block and not building them.
+ *
+ * ----
+ * TODO
+ * ----
+ *      - TestFail() routine should have better reporting
+ *      - consider a TestPass() routine
+ *
+ * ==========================================================================*/
+#include "tester.h"
+
+/* ****************************************************************************
+ * Test Selection
+ * ***************************************************************************/
+#define TEST_MEMORY_ENABLE 0
+
+/* ****************************************************************************
+ * Test Headers
+ * ***************************************************************************/
+#if TEST_MEMORY_ENABLE
+#include "test_memory.h"
+#endif
+
+/* ****************************************************************************
+ * FUNCTION: TestFail
+ * ------------------
+ * raise an alert to notify user that test failed!
+ * ***************************************************************************/
+void TestFail(void)
+{
+    while(1)
+    {
+        /*Poll here to indiciate failure*/
+    }
+
+    /*TODO: more elaborate reporting*/
+}
+
+/* ****************************************************************************
+ * FUNCTION: tester
+ * ----------------
+ * Main routine for all testing function
+ *
+ * ***************************************************************************/
+void tester(void)
+{
+
+	int ret;
+
+#if TEST_MEMORY_ENABLE
+    /*TODO: Memory testing*/
+#endif/*TEST_MEMORY*/
+}
+
--- src/C7C.Atamo.Dusty.Mote/main.c
+++ src/C7C.Atamo.Dusty.Mote/main.c
@@ -1,18 +1,74 @@
-/*
- * C7C.Atamo.Dusty.Mote.c
+ /* ============================================================================
+ * C7C.Atamo.Dusty.Mote.main
+ * -------------------------
+ * 
+ * -------
+ * PURPOSE
+ * -------
+ *      Function entry point
  *
- * Created: 2017-09-05 16:54:56
- * Author : kjph
- */ 
+ * ------------------
+ * DEVELOPMENT STATUS
+ * ------------------
+ *      Last Updated: 2017-09-04
+ *      Last Editor: Phan, J.
+ * 
+ *		Modified skeleton
+ *
+ * ----------------
+ * ADDITIONAL NOTES
+ * ----------------
+ *		Comment/Un-comment the #define DEBUG to compile in production or debug mode
+ *		With DEBUG defined, several tests will be performed and the main 
+ *		functionality will not.
+ *		Individual testing can be selected by defining/not-defining certain
+ *		tags; set "DEBUG Settings"
+ *
+ * ==========================================================================*/
 
+/* ****************************************************************************
+ * Standard Headers
+ * ***************************************************************************/
 #include <avr/io.h>
 
+/* ****************************************************************************
+ * DEBUG Settings
+ * ***************************************************************************/
 
-int main(void)
+/*Comment this out to turn off debug*/
+#define TEST_MODE 1
+
+#if TEST_MODE
+#include "tester/tester.h"
+#endif
+
+/* ****************************************************************************
+ * FUNCTION: system_init
+ * *********************
+ * To keep the main routine clean; these commands refer to initialisation 
+ * and should only be run during startup
+ *
+ * ***************************************************************************/
+void system_init (void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	continue;/*TODO*/
+}
+
+
+void main (void)
+{
+
+	system_init();
+
+#if TEST_MODE
+	while(1)
+	{
+		tester();
+	}
+#else
+	while(1)
+	{
+	}
+#endif
 }
 

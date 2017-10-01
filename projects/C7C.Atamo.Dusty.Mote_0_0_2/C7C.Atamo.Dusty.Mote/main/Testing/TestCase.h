e1def8a457ba5fc8e9762623c3f602d017914d6c
Jamie Phan <21326604@student.uwa.edu.au>
2017-10-01 22:50:41 +0800

    Doxygen Integration

--- projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/Testing/TestCase.h
+++ projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/Testing/TestCase.h
@@ -1,49 +1,66 @@
-/*
- * tester.h
+/*============================================================================*/
+/*!
+ * @file TestCase.h
  *
- * Created: 2017-09-21 08:31:48
- *  Author: kjph
- */ 
-
+ * @brief Unit Test Base Class
+ *
+ * This base class provides a unit testing module. Unit tests should derive from
+ * this base class and use the exposed functions to perform testing
+ * Testing must then be integrated to the tester.h module in the TLD
+ *
+ * @date 2017/10/01
+ * @author Jamie K. Phan
+ *
+ */
+/*============================================================================*/
 
 #ifndef TESTCASE_H_
 #define TESTCASE_H_
 
-/*! TestCase base class for unit-testing in Embedded App
-
-	This is the base class in which other unit-tests should derive from.
-	This class guarantees the methods run, t_fail, t_pass.
-*/
 class TestCase {
-	private:
-
-	protected:
-
 	public:
-		bool tctrl_stop_on_success;
+		
+		//=====================================
+		/*! TestCase base class for unit-testing in Embedded App
 
+			This is the base class in which other unit-tests should derive from.
+			This class guarantees the methods run, t_fail, t_pass.
+		*/
 		TestCase(void);
 
+		//=====================================
+		/*! Allows tester to specify if successful tests should hold the device
+		*/
+		bool tctrl_stop_on_success;
+
+		//=====================================
 		/*! Simple blink procedure for visual feedback on the DP
 		*/
 		void blink(void);
 
+		//=====================================
 		/*! Entry point for test cases
 		*/
 		virtual void run(void);
 
+		//=====================================
 		/*! Standardised routine for failures.
 
 			This routine holds on a while(1) with visual feedback
 		*/
 		void t_fail(void);
 
+		//=====================================
 		/*! Standardised routine for passes
 
 			This routine has the option to pass on success using
 			the attribute (tctrl_stop_on_success)
 		*/
 		void t_pass(void);
+
+	private:
+		
+	protected:
 };
 
 #endif /* TESTCASE_H_ */
---
+++ projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/Testing/TestDnUart.cpp
@@ -0,0 +1,6 @@
+/*
+ * TestDnUart.cpp
+ *
+ * Created: 2017-10-01 00:07:48
+ *  Author: kjph
+ */ 

06026db5cbd5330cd46bdc1f5a77afbed1be5419
Aaron Hurst <21325887@student.uwa.edu.au>
2017-10-05 19:39:44 +0800

    update frame.h based on sub-team mtg (02/10/2017); initial driver work

--- projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/sample/sensor.h
+++ projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/sample/sensor.h
@@ -10,9 +10,10 @@
 #define SENSOR_H_
 
 #include <stdint.h>
+#include "../globals.h" // SystemState
 
 /*******************************************************************************
- * Sensor lookup (most likely will be in sensor header)
+ * Sensor lookup
  ******************************************************************************/
 #define DP_SENS__TYPE_A 0x00
 
@@ -21,6 +22,13 @@
  ******************************************************************************/
 #define DP_SENS__STATUS_NOT_SET 0x00
 
+
 uint8_t sensor_read(uint8_t *buf);
+
+
+/*******************************************************************************
+ * Sensor Drivers
+ ******************************************************************************/
+uint8_t sensor_drive_type_a(uint8_t sensor_addr, uint8_t *buf);
 
 #endif /* SENSOR_H_ */

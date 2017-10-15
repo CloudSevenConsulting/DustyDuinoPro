c8ed578d54418f5ba57a8ec3445c350077bc7fa3
Jamie Phan <21326604@student.uwa.edu.au>
2017-10-16 00:28:30 +0800

    Add C API for DuinoPro CPP library
    Require c wrapper for DP's CPP functionaility (this is important
    for designing the interrupts)

---
+++ projects/C7C.Atamo.Dusty.Mote_0_0_2/C7C.Atamo.Dusty.Mote/main/dp_api.h
@@ -0,0 +1,22 @@
+/*============================================================================*/
+/*!
+ * @file dp_api.h
+ *
+ * @brief DuinoPro API for C functionality
+ *
+ * @date 2017/10/15
+ * @author Jamie K. Phan
+ *
+ */
+/*============================================================================*/
+
+#ifndef DP_API_H_
+#define DP_API_H_
+
+//=====================================
+/*! @brief Turn on board led
+*
+*/
+void dp_board_set_led(int);
+
+#endif
